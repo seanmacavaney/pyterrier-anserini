a680e7a774950c8ca27c1e0bbc602c674c282de7
Sean MacAvaney <sean.macavaney@gmail.com>
2026-02-22 14:18:37 +0000

    test gss

---
+++ tests/test_gss.py
@@ -0,0 +1,16 @@
+import unittest
+
+import pyterrier as pt
+import pyterrier_alpha as pta
+
+import pyterrier_anserini
+
+
+class TestGss(unittest.TestCase):
+    def test_gss_to_lucene(self):
+        # This tests a few things: that the anserini: parser is working, that the metadata adapter works, etc.
+        analyzer = pyterrier_anserini.J.StandardAnalyzer()
+        self.assertEqual('contents:hello contents:world', pyterrier_anserini.gss_to_lucene('hello world', analyzer).toString())
+        self.assertEqual('contents:hello +contents:world', pyterrier_anserini.gss_to_lucene('hello "world"', analyzer).toString())
+        self.assertEqual('+contents:"hello world"', pyterrier_anserini.gss_to_lucene('"hello world"', analyzer).toString())
+        self.assertEqual('+contents:"hello world" -contents:universe', pyterrier_anserini.gss_to_lucene('"hello world" -universe', analyzer).toString())
