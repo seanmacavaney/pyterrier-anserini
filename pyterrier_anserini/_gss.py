82956373ccc2acf7f5893cb3a8f223ee6bda7272
Sean MacAvaney <sean.macavaney@gmail.com>
2026-02-22 13:52:27 +0000

    drop gss domain_filter, add must_not_match

--- pyterrier_anserini/_gss.py
+++ pyterrier_anserini/_gss.py
@@ -2,7 +2,7 @@ from typing import List
 import re
 import pyterrier as pt
 
-_gss_re = re.compile(r'site:(?P<domain_filter>[.a-zA-Z0-9-]+)|"(?P<must_match>[^"]+)"|(?P<should_match>\S+)')
+_gss_re = re.compile(r'-(?P<must_not_match>\S+)|"(?P<must_match>[^"]+)"|(?P<should_match>\S+)')
 
 
 @pt.java.required
@@ -10,8 +10,7 @@ def gss_to_lucene(
     query: str,
     analyzer: object,
     *,
-    content_field: str = 'content',
-    domain_field: str = 'domain',
+    content_field: str = 'contents',
 ) -> object:
     """ Parses a query using a subset of Google Search Syntax (GSS) conventions into a Lucene query object.
 
@@ -19,29 +18,22 @@ def gss_to_lucene(
      - literal query terms: irs w9
      - terms that must match: irs "w9" (here w9 must occur in the document, but irs may or may not occur)
      - phrase matching: "irs w9" (here irs and w9 must occur in the document, and they must be adjacent to each other in the specified order)
-     - site filtering: site:irs.gov (matches documents from irs.gov and any subdomains, e.g. forms.irs.gov)
+     - negative term matching: irs w9 -1040 (1040 must NOT occur in the document, but irs and w9 may or may not occur)
 
-    The method should mostly behave similarly to Google Search, but a few known differences are present:
+    The method should behave somewhat similarly to Google Search, but a few known differences are present:
+     - Lots of features are missing -- e.g., domain filtering (site:irs.gov), etc. These are treated as normal query terms.
      - The parsing almost certainly doesn't work the same exact way, especially for edge cases
      - Quoted matches are exact in Google Search, here the analyzer is applied (e.g., with stemming and such)
+     - Similar story for negations -- the analyzer is applied
     """
     query_builder = J.BooleanQueryBuilder()
     for match in _gss_re.finditer(query):
-        if match.group('domain_filter'):
-            domain_suffix = match.group('domain_filter').lower()
-            if domain_suffix.startswith('.'):
-                # example: site:.gov -> matches any domain that ends with .gov (freedom.gov, eagle.freedom.gov, etc.)
-                query_builder.add(J.WildcardQuery(J.Term(domain_field, f'*{domain_suffix}')), J.Occur.FILTER)
-            else:
-                # example: site:freedom.gov -> matches freedom.gov and any subdomains (eagle.freedom.gov)
-                domain_filter_builder = J.BooleanQueryBuilder()
-                domain_filter_builder.add(J.TermQuery(J.Term(domain_field, domain_suffix)), J.Occur.SHOULD)
-                domain_filter_builder.add(J.WildcardQuery(J.Term(domain_field, f'*.{domain_suffix}')), J.Occur.SHOULD)
-                query_builder.add(domain_filter_builder.build(), J.Occur.FILTER)
+        if match.group('must_not_match'):
+            for token in _tokenize(match.group('must_not_match'), analyzer, content_field=content_field):
+                query_builder.add(J.TermQuery(J.Term(content_field, token)), J.Occur.MUST_NOT)
 
         elif match.group('must_match'):
             tokens = _tokenize(match.group('must_match'), analyzer, content_field=content_field)
-
             if len(tokens) == 1:
                 # single-term MUST
                 query_builder.add(J.TermQuery(J.Term(content_field, tokens[0])), J.Occur.MUST)
@@ -80,7 +72,6 @@ J = pt.java.JavaClasses(
     PhraseQueryBuilder = 'org.apache.lucene.search.PhraseQuery$Builder',
     Occur = 'org.apache.lucene.search.BooleanClause$Occur',
     TermQuery = 'org.apache.lucene.search.TermQuery',
-    WildcardQuery = 'org.apache.lucene.search.WildcardQuery',
     Term = 'org.apache.lucene.index.Term',
     CharTermAttribute = 'org.apache.lucene.analysis.tokenattributes.CharTermAttribute',
 )
